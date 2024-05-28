497873d95ea8b63b5b98fea3a5cc80a303f28193
miyanao <k023g0050@g.neec.ac.jp>
2024-05-29 04:03:54 +0900

    02_14_確認課題 一部修正

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -20,8 +20,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	//win->CreateGameWindow(L"LE2A_17_ミヤザワ_ナオキ_AL3");
-	win->CreateGameWindow(L"LE2A_17_セナ_ミンス_AL3");
+	win->CreateGameWindow(L"LE2A_17_ミヤザワ_ナオキ_AL3");
+	
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
